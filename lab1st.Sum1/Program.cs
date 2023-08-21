abf7ca31f64b908d998cf3cc107f8a6bde417fcd
hp <hp@DESKTOP-75BFR6A>
2023-08-21 22:42:31 +0530

    Add project files.

---
+++ lab1st.Sum1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1st.Sum1
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[5];
+            float avg = 0f;
+            Console.WriteLine("Enter the element of array : ");
+            for (int i = 0; i < 5; i++) Arr[i] = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += Arr[i];
+            }
+            avg = sum / 5;
+            Console.WriteLine("The average of all elements of array : " + avg);
+            Console.ReadLine();
+        }
+    }
+}
